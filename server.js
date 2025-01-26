d0f3233a9d83e00c055ced94a71c31f700aa7e27
s224394372 <s224394372@deakin.edu.au>
2025-01-26 09:50:31 +0530

    Order Page APIs basic model structure added.

--- server.js
+++ server.js
@@ -3,8 +3,6 @@ let express =  require('express');
 let { Server } = require ('socket.io');
 let path = require ('path');
 let { fileURLToPath } = require ('url');
-let router = require ('./routers/router.js');
-let otherPageRouter = require ('./routers/otherPageRouter.js'); // Ensure your router file uses ES modules
 let client = require('./dbConnection.js');
 let Offer = require ('./controllers/offersController.js');
 
@@ -24,10 +22,6 @@ app.use(express.urlencoded({ extended: false }));
 // Serve the "img" folder for image assets
 app.use('/img', express.static(path.join(__dirname, 'img')));
 
-// Use the router for '/admin' routes
-app.use('/', router);
-app.use('/', otherPageRouter);
-
 // Dynamic route to serve HTML files in the subpages folder
 app.get('/subpages/:folder/:file.html', (req, res) => {
     const { folder, file } = req.params;
@@ -42,9 +36,11 @@ app.get('/:page.html', (req, res) => {
     res.sendFile(filePath);
 });
 
-
+let router = require ('./routers/router.js');
 let menRouter = require('./routers/menPageRouter');
 let womenRouter = require('./routers/womenPageRouter');
+let otherPageRouter = require ('./routers/otherPageRouter.js');
+let orderPageRouter = require ('./routers/orderPageRouter.js');
 let cartPageRouter = require('./routers/cartPageRouter');
 
 
@@ -52,8 +48,11 @@ const http = require('http').Server(app);
 // var port = 8080;
 require('./dbConnection');
 
+app.use('/', router);
 app.use(menRouter);
 app.use(womenRouter);
+app.use('/', otherPageRouter);
+app.use(orderPageRouter);
 app.use('/controllers', express.static(path.join(__dirname, 'controllers')));
 app.use('/partials', express.static(path.join(__dirname, 'partials')));
 app.use('/subpages', express.static(path.join(__dirname, 'subpages')));
